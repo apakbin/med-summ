f4c5089ff124ed09df1cc97f6c332d02f4afcfb7
Arash Pakbin <a.pakbin@cse-stmi-s1.cse.tamu.edu>
2024-09-08 02:22:23 -0500

    put everything in main()

--- src/main.py
+++ src/main.py
@@ -2,12 +2,16 @@ import transformers
 import torch
 import utils
 
-config = utils.get_config()
-utils.set_visible_cuda_devices(config)
+def main():
+    config = utils.get_config()
+    utils.set_visible_cuda_devices(config)
 
 
-pipeline = transformers.pipeline(
-    "text-generation", model=config.model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
-)
+    pipeline = transformers.pipeline(
+        "text-generation", model=config.model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
+    )
 
-print (pipeline("Hey how are you doing today?", max_new_tokens = 20))
+    print (pipeline("Hey how are you doing today?", max_new_tokens = 20))
+
+if __name__=="__main__":
+    main()
