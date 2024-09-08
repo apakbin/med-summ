d5cd5fe026cd0795a25e1675c0d34443d20a1519
Arash Pakbin <a.pakbin@cse-stmi-s1.cse.tamu.edu>
2024-09-08 02:20:35 -0500

    moved more Fs to utils.py

--- src/main.py
+++ src/main.py
@@ -2,13 +2,9 @@ import transformers
 import torch
 import utils
 
-import os
-from huggingface_hub import login
+config = utils.get_config()
+utils.set_visible_cuda_devices(config)
 
-login(token = os.environ['hf_token'])
-
-config                             = utils.get_config()
-os.environ["CUDA_VISIBLE_DEVICES"] = str(config.gpus)
 
 pipeline = transformers.pipeline(
     "text-generation", model=config.model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
 import os
 import json
+from huggingface_hub import login as _hf_login
 from types import SimpleNamespace
+
+
+def hf_login():
+    _hf_login(token = os.environ['hf_token'])
+
+def set_visible_cuda_devices(config):
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(config.gpus)
 
 def get_config():
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')) as f:
