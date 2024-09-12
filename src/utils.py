3a27d38424cbf0b868890fc0fa30c5d5c916b5c9
Arash Pakbin <a.pakbin@cse-stmi-s2.engr.tamu.edu>
2024-09-11 19:48:13 -0500

    added chunks

--- src/utils.py
+++ src/utils.py
@@ -35,6 +35,13 @@ def load_pickle(file_path):
         return pickle.load(handle)
 
 
+#https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
 def cache(func):
     @functools.wraps(func)
 
